200a749e65e13a98f68cfbc3f1067e8938d1c3af
Lucas Miná <lucas.mina@ccc.ufcg.edu.br>
2022-03-22 21:20:24 -0300

    Change Class Access Modifier (ApiConstants)

--- telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/ApiConstants.java
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/ApiConstants.java
@@ -6,6 +6,10 @@ package org.telegram.telegrambots.meta;
  * Constants needed for Telegram Bots API
  */
 public class ApiConstants {
+    private ApiConstants() {
+        
+    }
+
     public static final String BASE_URL = "https://api.telegram.org/bot";
     public static final int GETUPDATES_TIMEOUT = 50;
     public static final String RESPONSE_FIELD_OK = "ok";
